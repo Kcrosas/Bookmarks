9bc8f8e50a0438a9f79e5363b8906e80fb86b797
Kenneth Rosas <kencrosas@gmail.com>
2021-11-08 23:33:26 -0500

    Defined accesses to individual bookmark attributes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const db = require("./db");
 const data = db.models.Data;
 
-//Defaults to bookmarks
+//Defaults(redirects) to /bookmarks
 app.get("/", async (req, res, next) => {
   res.redirect("/bookmarks");
 });
@@ -11,12 +11,31 @@ app.get("/", async (req, res, next) => {
 app.get("/bookmarks", async (req, res, next) => {
   try {
     const datas = await data.findAll();
-    res.send(datas);
+    res.send(`
+    <ul>
+    ${datas
+      .map(
+        (ele, idx, arr) => `
+        <li>
+        Name: ${arr[idx].name}<br>
+        URL: ${arr[idx].URL}<br>
+        Category: ${arr[idx].category}<br><br>
+        </li>
+        `
+      )
+      .join("")}
+    
+    
+    
+    
+    </ul>
+    `);
   } catch (error) {
     next(error);
   }
 });
 
+//Seeds data table and sets port to 3000
 const init = async () => {
   await db.syncAndSeed();
   const port = process.env.PORT || 3000;
